7daed09e1bd43293c0e865904d436c379deab19a
DEBASMITROY2002 <ddruk2018@gmail.com>
2025-12-15 14:13:31 +0530

    iam refactored

--- sa.tf
+++ sa.tf
@@ -4,12 +4,3 @@ resource "google_service_account" "service_account" {
   display_name = local.service_account_display_name
   project      = local.gcp_project_id
 }
-
-# IAM Policy Bindings
-resource "google_project_iam_member" "service_account_iam" {
-  for_each = toset(local.iam_roles)
-  project  = local.gcp_project_id
-  role     = each.value
-  member   = "serviceAccount:${google_service_account.service_account.email}"
-  depends_on = [ google_project_service.apis , google_service_account.service_account ]
-}
